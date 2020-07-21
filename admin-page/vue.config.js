d4960f0e7c1bbf7b096de512c914368614f9dabb
RyanWang <wb38113@ly.com>
2020-07-21 12:00:55 +0800

    modify application.yml

--- admin-page/vue.config.js
+++ admin-page/vue.config.js
@@ -24,7 +24,7 @@ module.exports = {
    * In most cases please use '/' !!!
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
-  publicPath: '/',
+  publicPath: '/admin',
   outputDir: '../admin-api/src/main/resources/static',
   assetsDir: '',
   lintOnSave: process.env.NODE_ENV === 'development',
