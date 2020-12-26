0931b739e97610d1a736eed23c11be32e5ab5ac9
王兵 <wangbing@wangbingdeMacBook-Pro.local>
2020-12-26 12:39:41 +0800

    modify logs path

--- admin-api/src/main/java/com/bing/admin/AdminApiApplication.java
+++ admin-api/src/main/java/com/bing/admin/AdminApiApplication.java
@@ -2,7 +2,6 @@ package com.bing.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
